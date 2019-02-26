ab81ece39f3be7b968df5f755a5fbea3c25d7482
Wolfteinter <onderfra@gmail.com>
2019-02-26 00:22:18 -0600

    Se modifica para que se acepte todo simbolo, corregir el problema numCaracter

--- main.cpp
+++ main.cpp
@@ -4,11 +4,10 @@ using namespace std;
 int main(){
     Extractor extractor("code.txt");
     LexicoAnalysis analy;
-    cout<<extractor.extract()<<endl;
     //vector<string> errors = analy.analysis(extractor.extractTokens());
     //cout<<"------"<<endl;
-    //for(string s: extractor.extractTokens()){
-    //    cout<<s<<endl;
-    //}
+    for(string s: extractor.extractTokens()){
+        cout<<s<<endl;
+    }
     return 0;
 }
